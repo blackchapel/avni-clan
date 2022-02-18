805f1eb4a330cf717ff71c12d57bc6281ccad499
kc <kc.dark@protonmail.com>
2022-02-19 03:47:29 +0530

    Added climate data api route

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ const express = require("express");
 const morgan = require('morgan');
 const cors = require("cors");
 const db = require('./connection');
-const authRoute = require('./routes/auth'); 
 
 // Initializing an express app
 const app = express();
 
+// Server Port
 const PORT = process.env.PORT || 5001;
 
 // Formatting incoming data
@@ -17,8 +17,13 @@ app.use(express.json());
 // Logging
 app.use(morgan('dev'));
 
+// Importing Routes
+const authRoute = require('./routes/auth');
+const apidata = require('./routes/api'); 
+
 // Routes
 app.use('/api/user', authRoute);
+app.use('/api/data', apidata);
 
 app.get('/api', (req, res) => {
     res.json({ message: "Hello from server!" });
