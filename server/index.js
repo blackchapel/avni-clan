481902b2ea5f7c5817f72f27e408f27695c7f088
Vidhita Pai <optimus@vd.local>
2022-02-19 02:58:29 +0530

    Added auth route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const morgan = require('morgan');
 const cors = require("cors");
 const db = require('./connection');
+const authRoute = require('./routes/auth'); 
 
 // Initializing an express app
 const app = express();
@@ -15,6 +16,9 @@ app.use(express.json());
 
 // Logging
 app.use(morgan('dev'));
+
+// Routes
+app.use('/api/user', authRoute);
 
 app.get('/api', (req, res) => {
     res.json({ message: "Hello from server!" });
