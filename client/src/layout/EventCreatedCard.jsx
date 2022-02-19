35d86b59dc5cd2c0e6b13f96c7fceaaf8f9c7426
Yashpb1513 <yashbrahmbhatt26@gmail.com>
2022-02-19 17:12:54 +0530

    Dashboard edited

---
+++ client/src/layout/EventCreatedCard.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  Grow,
+  Grid,
+  Typography,
+  CardContent,
+  CardActions,
+  Button,
+  Paper,
+} from "@mui/material";
+function EventCreatedCard() {
+  return (
+    
+                <Grid item>
+                  <Paper>
+                    <CardContent sx={{ width: "350px" }}>
+                      <Grid
+                        container
+                        direction="row"
+                        justifyContent="space-between"
+                        alignItems="flex-start"
+                      >
+                        <Grid item>
+                          <Typography
+                            sx={{ fontSize: 25 }}
+                            color="text.secondary"
+                            gutterBottom
+                          >
+                            Event Name
+                          </Typography>
+                        </Grid>
+                        <Grid item>
+                          <Typography
+                            sx={{ fontSize: 18 }}
+                            color="secondary"
+                            gutterBottom
+                          >
+                            OnGoing
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                      <Typography variant="h5" component="div"></Typography>
+                      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        Date: and Time:
+                      </Typography>
+                      <Typography variant="body2">
+                        well meaning and kindly.
+                        <br />
+                        {'"a benevolent smile"'}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="medium" variant="contained">Start</Button>
+                      <Button size="medium" variant="contained" color="secondary">End</Button>
+                    </CardActions>
+                  </Paper>
+                </Grid>
+  );
+}
+
+export default EventCreatedCard;
