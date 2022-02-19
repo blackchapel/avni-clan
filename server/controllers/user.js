1bbbe7bd7a8f72394dd2bf42b8780ef5e6f1accb
kc <kc.dark@protonmail.com>
2022-02-19 15:57:51 +0530

    User Dasboard & User Leaderboard Controller

---
+++ server/controllers/user.js
@@ -0,0 +1,51 @@
+// Importing Modules
+const User = require('../models/user.js');
+const { 
+    removeSensitiveData, 
+    removeUnnecessaryData 
+} = require('../utility/functions'); 
+
+// Get User Data
+const getUser = async (req, res) => {
+    
+    try {
+        let currentUser = await User.findById(req.user._id).populate('eventscreated').populate('eventsjoined');
+    
+        let eventscreated = currentUser.eventscreated
+        let eventsjoined = currentUser.eventsjoined;
+
+        currentUser = removeSensitiveData(currentUser);
+
+        res.status(200).json({
+            user: currentUser,
+            credits: currentUser.credits,
+            eventscreated: eventscreated,
+            eventsjoined: eventsjoined
+        });
+    } catch(error) {
+        res.status(400).json({
+            message: error.message
+        });
+    }
+};
+
+const findRanking = async (req, res) => {
+    try {
+        let users = await User.find();
+        users.sort((a, b) => (a.credits < b.credits) ? 1 : -1);
+
+        res.status(200).json({
+            data: users
+        });
+    } catch(error) {
+        res.status(400).json({
+            message: error.message
+        });
+    }
+};
+
+// Exporting Modules
+module.exports = { 
+    getUser,
+    findRanking
+};
